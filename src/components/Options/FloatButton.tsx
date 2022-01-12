cca70bd82730c1c9f6bc9e419f414c0c798883ab
1tang2bang92 <shpark0906@naver.com>
2022-01-12 17:04:57 +0900

    옵션선택 버튼 FLOAT 시키기

--- src/components/Options/FloatButton.tsx
+++ src/components/Options/FloatButton.tsx
@@ -1,7 +1,22 @@
-import { Button, ButtonTypeMap, ExtendButtonBase} from '@mui/material'
+import styled from '@emotion/styled'
+import { Fab, ButtonTypeMap, ExtendButtonBase} from '@mui/material'
+import SettingsIcon from '@mui/icons-material/Settings';
 
 const FloatButton: ExtendButtonBase<ButtonTypeMap> = (props: any) => {
-  return <Button {...props}> + </Button>
+  return (
+    <FloatBtn 
+      color='primary'
+      {...props}
+    > 
+      <SettingsIcon /> 
+    </FloatBtn>
+  )
 }
+
+const FloatBtn = styled(Fab)`
+  position: fixed; 
+  right: 10px; 
+  bottom: 10px;
+`
 
 export default FloatButton
